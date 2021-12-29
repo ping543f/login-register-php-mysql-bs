231b3b6f0471cf86ad946255f30dee1e79d90044
ping 543f <md.saefullah@gmail.com>
2021-12-29 09:51:27 +0800

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,9 @@
     $username='root';
     $password='';
     $dbname='login_db';
+    // $port='3307';
     $conn=mysqli_connect($url,$username,$password,$dbname);
+    // $conn=mysqli_connect($url,$username,$password,$dbname,$port);
     if(!$conn){
-        die('Could not Connect to Database. Following error occured:' .mysql_error());
+        die('Could not Connect to Database. Following error occured:' .mysqli_error());
     }
